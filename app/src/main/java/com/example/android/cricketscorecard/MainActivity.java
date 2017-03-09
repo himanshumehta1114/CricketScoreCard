c1f6910d718c681b93fbfccb5a16ecbc5506cda7
himanshu <himanshumehta1114@gmail.com>
2017-03-09 22:51:43 +0530

    created UI

---
+++ app/src/main/java/com/example/android/cricketscorecard/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.cricketscorecard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    void displayNumber(int num){
+
+        TextView textview = (TextView) findViewById(R.id.bowlsFieldA);
+
+
+    }
+
+    return increment(int number)
+    {
+        number++;
+        return number;
+    }
+}
